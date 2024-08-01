3a6d8c8d48211a7889e779dc4369b2d797566e79
unknown <victornnamani651@gmail.com>
2024-08-01 04:08:31 +0100

    Cover (start) Page)
    created the necessary files for the project, and my first task was creating a cover page with a start button, both of which are animated, and meant to display the page behind the cover, did this cover not for anything but for css' sake,just to toy around with it a bit

---
+++ index.js
@@ -0,0 +1,6 @@
+// To animate cover page slide out when start button is clicked
+let coverPage = document.getElementById("cover");
+let startButton = document.getElementById("startbtn");
+startButton.addEventListener("click", () => {
+  coverPage.classList.toggle("animate__slideOutUp");
+});

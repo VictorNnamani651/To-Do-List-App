c66502321d3451adb0bf690d8914c8c4e699b61f
unknown <victornnamani651@gmail.com>
2024-08-20 08:48:33 +0100

    added logic to the 'add task' button to enable it add new tasks
    made a few adjustments to the code as well, changing a lot of hard coded features into slightly more dynamic ones for each task, for instance the checkbox state and the display state of the task description for each task

--- userPage.js
+++ userPage.js
@@ -1,71 +1,90 @@
-// check button logic =========================================
-// this code is responsible for controlling the check box (to display as checked or unchecked)
-// added an event listener to the check icon to enable it change state on click
-let checkBox = document.getElementById("checkBox");
-checkBox.addEventListener("click", () => {
-  if (checkBox.classList.contains("fa-square-o")) {
-    checkBox.classList.remove("fa-square-o");
-    checkBox.classList.add("fa-check-square-o");
+// check button logic ==================================================
+// Function for the checkBox icon to enable it change state on click
+function change_icon_state(elem) {
+  if (elem.classList.contains("fa-square-o")) {
+    elem.classList.remove("fa-square-o");
+    elem.classList.add("fa-check-square-o");
   } else {
-    checkBox.classList.remove("fa-check-square-o");
-    checkBox.classList.add("fa-square-o");
+    elem.classList.remove("fa-check-square-o");
+    elem.classList.add("fa-square-o");
   }
-});
+}
 // ===================================================================
 
-// description button logic========================================
-let descriptionButton = document.getElementsByClassName("desc-btn")[0];
-let descriptionIcon = document.getElementsByClassName("desc-icon")[0];
-let descriptionDiv = document.getElementsByClassName("description-a")[0];
-
-// function to check the state of description icon(to know if the icon is facing down or up)================
-function check_description_icon_state() {
-  if (descriptionIcon.classList.contains("fa-chevron-up")) {
+// function to check the state of description drop-down icon(to know if the icon is facing down or up)=================================================
+function check_description_icon_state(elem) {
+  if (elem.querySelector(".desc-icon").classList.contains("fa-chevron-up")) {
     return true;
-  } else if (descriptionIcon.classList.contains("fa-chevron-down")) {
+  } else if (
+    elem.querySelector(".desc-icon").classList.contains("fa-chevron-down")
+  ) {
     return false;
   }
 }
 // =========================================================================================
 
-// function to change the description icon when the description button is clicked=======================
+// function to change the description drop-down icon when the description button is clicked
 // NOTE == True means the icon arrow is facing up & False means it is facing down==========================
 let up_or_down;
-function change_description_icon() {
-  if (check_description_icon_state() === true) {
-    descriptionIcon.classList.toggle("fa-chevron-up");
-    descriptionIcon.classList.toggle("fa-chevron-down");
+function change_description_icon(elem) {
+  if (check_description_icon_state(elem) === true) {
+    elem.querySelector(".desc-icon").classList.toggle("fa-chevron-up");
+    elem.querySelector(".desc-icon").classList.toggle("fa-chevron-down");
     up_or_down = "down";
     return up_or_down;
-  } else if (check_description_icon_state() === false) {
-    descriptionIcon.classList.toggle("fa-chevron-down");
-    descriptionIcon.classList.toggle("fa-chevron-up");
+  } else if (check_description_icon_state(elem) === false) {
+    elem.querySelector(".desc-icon").classList.toggle("fa-chevron-down");
+    elem.querySelector(".desc-icon").classList.toggle("fa-chevron-up");
     up_or_down = "up";
     return up_or_down;
   }
 }
 
-// function to hide or display the description based on the state of the description icon(up for hide and down for display)=======
-function hide_or_show_description() {
+// function to hide or display the task description based on the state of the description drop-down icon(up for hide and down for display)=======
+function hide_or_show_description(elem) {
   if (up_or_down == "up") {
-    descriptionDiv.classList.toggle("hide");
+    elem.parentElement.parentElement.parentElement
+      .querySelector("div.description-a")
+      .classList.toggle("hide");
   } else if (up_or_down == "down") {
-    descriptionDiv.classList.toggle("hide");
+    elem.parentElement.parentElement.parentElement
+      .querySelector("div.description-a")
+      .classList.toggle("hide");
   }
 }
 // ==========================================================================================================
 
-// the code below is to add a trigger for the  change_description_icon function and the hide_or_show_description function to the description button==
-descriptionButton.addEventListener("click", () => {
-  change_description_icon();
-  hide_or_show_description();
-});
+// the function below is to add a trigger for the  change_description_icon function and the hide_or_show_description function to the description button==============================
+function description_state(elem) {
+  change_description_icon(elem);
+  hide_or_show_description(elem);
+}
 // ================================================================================
 
-// // Add_task button logic==============================================
-// let add_task_button = document.getElementById("add_task");
-// let new_task_div = document.createElement
+// // Code to add a new task to the app via the DOM=======================================================
+let add_task_button = document.getElementById("add_task");
 
-// function create_new_task(){
-  
-// }
+let new_task;
+add_task_button.addEventListener("click", () => {
+  new_task = document.createElement("div");
+  new_task.classList.add("task");
+  new_task.innerHTML = `<div class="task">
+          <div class="title-a">
+            <div class="firstHalf">
+              <i class="fa fa-square-o checkBox" onclick="change_icon_state(this)"></i>
+              <div class="task-title">Task Title</div>
+            </div>
+            <div class="secondHalf">
+              <div class="task-due-date">08/05/2024</div>
+              <button class="desc-btn" onclick="description_state(this)">
+                <i class="fa fa-chevron-down desc-icon"></i>
+              </button>
+            </div>
+          </div>
+          <div class="description-a hide">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga sint
+            modi nemo a ipsam maxime et voluptatem iusto nulla vel.
+          </div>
+        </div>`;
+  document.getElementById("tasks").appendChild(new_task);
+});
